a1e097e08330e904fa01bd73f86792d8961b46f7
Ruslan <ruslan.recebli@correcttechno.com>
2023-01-06 13:29:31 +0400

    robot arm grbl control

---
+++ src/main.ino
@@ -0,0 +1,17 @@
+
+
+/*
+  Supported hardware:
+    Arduino Duemilanove
+    Arduino Uno
+    Arduino Mega 2560 (Limited Testing)
+
+*/
+
+#include <SPI.h>
+#include <Arduino.h>
+
+
+#include <grbl.h>
+
+// Do not alter this file!
